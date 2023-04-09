14f96adfd728af018b87210eb6117d15f17ac184
vladus80 <utru@inbox.ru>
2023-04-09 16:52:05 +0300

    Проверка комита

--- AndroidStudioProjects/PlayerVL/app/src/main/java/com/example/myapplication/MyAdapter.java
+++ AndroidStudioProjects/PlayerVL/app/src/main/java/com/example/myapplication/MyAdapter.java
@@ -105,6 +105,8 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> impl
         });
 
 
+        /* Проверка*/
+
         holder.btnStarLike.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
